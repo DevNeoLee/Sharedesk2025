4bdcc362d9c6c68eb72b8330380f67fe38a11a1b
Justin Lee <devneolee@gmail.com>
2025-07-12 11:04:04 -0700

    improve JS loading with calander on reservations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < Devise::SessionsController
         if user.email == 'demo@email.com'
           flash[:notice] = "Welcome to the demo! You are now logged in as a demo user. Feel free to explore all features."
         else
-          flash[:notice] = "Welcome back, #{user.name}! You have successfully signed in."
+          flash[:notice] = "Welcome, #{user.name}! You have successfully signed in."
         end
       end
     end
