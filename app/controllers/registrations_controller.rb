194ef181ce8019b37dc8e0c5312a9f9f04c21239
Justin Lee <devneolee@gmail.com>
2025-07-11 22:51:18 -0700

    style edit profile page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,12 @@ class RegistrationsController < Devise::RegistrationsController
         def update_resource(resource, params) 
             resource.update_without_password(params)
         end
+
+    def destroy
+        # Delete all rooms associated with the user first
+        current_user.rooms.destroy_all
+        
+        # Then delete the user account
+        super
+    end
 end
